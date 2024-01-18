41bfaf921bb73e6115b64a21524276b45927f7d7
Your Name <you@example.com>
2024-01-18 10:10:29 +0500

    Initial commit

--- src/js/game.js
+++ src/js/game.js
@@ -1,15 +1,15 @@
 import { Character } from "./domain.js";
- export default class Game {
+export default class Game {
     start() {
       console.log('game started');
     }
   }
   
-  class GameSavingData {
+  export class GameSavingData {
   }
   
-  function readGameSaving() {
+  export function readGameSaving() {
   }
   
-  function writeGameSaving() {
+  export function writeGameSaving() {
   }
